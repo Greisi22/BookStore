69502c68b80eabcd31867e7d28537a39529d3416
David Keci <dkeci21@epoka.edu.al>
2023-10-19 12:03:45 +0200

    Davidi

--- src/Librarian/A__Main_Project.java
+++ src/Librarian/A__Main_Project.java
@@ -49,6 +49,7 @@ public class A__Main_Project  extends Application implements Serializable  {
 	public static void main(String[] args) {
 		System.out.println("David Greiso");
 		Application.launch(args);
+		System.out.println("Greisi");
 	}
 
 }

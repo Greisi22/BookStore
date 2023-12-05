04a78ff58b76d4673ff7d9fe9c33b600c868b8ad
EraMulla <emulla21@epoka.edu.al>
2023-12-05 12:42:20 +0100

    Prova

--- src/Librarian/A__Main_Project.java
+++ src/Librarian/A__Main_Project.java
@@ -13,10 +13,11 @@ public class A__Main_Project  extends Application  {
 	public  void start(Stage stage){
 
  		B__Log_in.Log_inn(stage);
+
 	}
 
 	public static void main(String[] args) {
-  		System.out.println("System started successfully...");
+  		System.out.println("System started successfully....");
 		Application.launch(args);
 	}
 

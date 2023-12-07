837bef1eaa47659f32758c17fa66601be20c4da9
greisi jaho <gjaho21@epoka.edu.al>
2023-12-08 00:47:19 +0100

    Doing some changes

--- src/Librarian/A__Main_Project.java
+++ src/Librarian/A__Main_Project.java
@@ -17,7 +17,7 @@ public class A__Main_Project  extends Application  {
 	}
 
 	public static void main(String[] args) {
-  		System.out.println("System started successfully...David");
+  		System.out.println("System started successfully...Greisi");
 		Application.launch(args);
 	}
 

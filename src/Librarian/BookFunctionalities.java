66cb9d56473d8518a8fb3d6d9ddf823501bf9005
David Keci <dkeci21@epoka.edu.al>
2023-12-15 14:32:07 +0100

    fixing book functionallities

--- src/Librarian/BookFunctionalities.java
+++ src/Librarian/BookFunctionalities.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 
 public class BookFunctionalities {
+    private static final String path = "src/EncodedInformation/Books.dat";
     public static <E> ArrayList<E> getBooks( String path) {
         ArrayList<E> list = new ArrayList<>();
         FileInputStream fis;
@@ -28,56 +29,23 @@ public class BookFunctionalities {
         return list;
     }
 
+
+
+
     //..............................................................................
 
 
     //.........................................................................
 
-    public static ArrayList<Zh_Books> UpdateBook(Zh_Books b1) {
-        FileInputStream fis;
-        ArrayList<Zh_Books> newBooks1 = new ArrayList<>();
-        if (Files.exists(Path.of("src/EncodedInformation/Books.dat"))) {
-            System.out.println("Directory is valid and exists.");
-        } else {
-            System.out.println("Directory does not exist or is not a valid directory.");
-        }
-        try {
-            fis = new FileInputStream("src/EncodedInformation/Books.dat");
-            ObjectInputStream objis = new ObjectInputStream(fis);
-
-            while (true) {
-                try {
-                    Zh_Books obj = (Zh_Books) objis.readObject();
-
-                    if (obj.getISBN().equals(b1.getISBN())) {
-                        obj = b1;
-                    }
-                    newBooks1.add(obj);
-                } catch (EOFException e) {
-                    break;
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
+    public static ArrayList<Zh_Books> UpdateBook(Zh_Books bookupdate) {
+        ArrayList<Zh_Books> books = getBooks(path);
+        for (int i=0;i<books.size();i++){
+            if (books.get(i).getISBN().equals(bookupdate.getISBN())) {
+                books.set(i, bookupdate);
             }
-
-            objis.close();
-
-            // Write updated data back to the file
-            FileOutputStream fos = new FileOutputStream("src/EncodedInformation/Books.dat");
-            ObjectOutputStream objos = new ObjectOutputStream(fos);
-            for (Zh_Books book : newBooks1) {
-                objos.writeObject(book);
-            }
-            objos.close();
-
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e) {
-
-            return newBooks1;
         }
-
-        return newBooks1;
+        writeBooksInFile(books);
+       return books;
     }
 
 
@@ -85,7 +53,7 @@ public class BookFunctionalities {
     //.....................................................
 
 
-    public static void createBook(ArrayList<Zh_Books> newBooks1){
+    public static void writeBooksInFile(ArrayList<Zh_Books> newBooks1){
         FileOutputStream out;
         try {
             out = new FileOutputStream("src/EncodedInformation/Books.dat");

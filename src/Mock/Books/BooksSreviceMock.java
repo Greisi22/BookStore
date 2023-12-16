f10f0776a7d043244807505a75a90463db6d8527
David Keci <dkeci21@epoka.edu.al>
2023-12-16 13:37:53 +0100

    prova

--- src/Mock/Books/BooksSreviceMock.java
+++ src/Mock/Books/BooksSreviceMock.java
@@ -1,14 +1,28 @@
 package Mock.Books;
 
 import Librarian.BookService;
+import Librarian.Zh_Books;
 
 import java.util.ArrayList;
 
 public class BooksSreviceMock extends BookService {
     @Override
     public  <E> ArrayList<E> getBooks(String path) {
+        ArrayList<Zh_Books> books = new ArrayList<>();
+        Zh_Books book1 = new Zh_Books();
+        Zh_Books book2 = new Zh_Books();
+        book1.setISBN("11");
+        book1.setQuanity(10);
+        book2.setISBN("22");
+        book2.setQuanity(20);
+        books.add(book1);
+        books.add(book2);
 
-        return null;
+        return (ArrayList<E>) books;
+    }
+    @Override
+    public  void writeBooksInFile(ArrayList<Zh_Books> newBooks1) {
+
     }
 
 }
--- src/Test/Librarian/Books/BookFunctionalitiesTest.java
+++ src/Test/Librarian/Books/BookFunctionalitiesTest.java
@@ -3,6 +3,7 @@ package Test.Librarian.Books;
 import Librarian.BookFunctionalities;
 import Librarian.Zh_Books;
 import Librarian.BookService;
+import Mock.Books.BooksSreviceMock;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -17,58 +18,45 @@ import static org.mockito.Mockito.doNothing;
 
 
 class BookFunctionalitiesTest {
-//    @TempDir
-//    static File tempFolder;
-//
-//    @AfterEach
-//    void cleanup() {
-//        tempFolder.delete();
-//    }
-//
-//    @Test
-//    void testGetBooks() throws IOException {
-//        File tempFile = new File(tempFolder, "testBooks.dat");
-//
-//        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(tempFile))) {
-//            Zh_Books book1 = new Zh_Books("Book 1", "Author 1", 2023);
-//            Zh_Books book2 = new Zh_Books("Book 2", "Author 2", 2024);
-//            objectOutputStream.writeObject(book1);
-//            objectOutputStream.writeObject(book2);
-//        }
-//
-//        ArrayList<Zh_Books> bookList = BookService.getBooks("testBooks.dat");
-//        assertEquals(2, bookList.size());
-//        assertEquals("Book 1", bookList.get(0).getTitle());
-//        assertEquals("Author 2", bookList.get(1).getISBN());
-//        assertEquals(2024, bookList.get(1).getQuanity());
-//    }
+    @TempDir
+    static File tempFolder;
+
+    @AfterEach
+    void cleanup() {
+        tempFolder.delete();
+    }
+
+    @Test
+    void testGetBooks() throws IOException {
+        File tempFile = new File(tempFolder, "testBooks.dat");
+        BookService bookService = new BookService();
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(tempFile))) {
+            Zh_Books book1 = new Zh_Books("Book 1", "Author 1", 2023);
+            Zh_Books book2 = new Zh_Books("Book 2", "Author 2", 2024);
+            objectOutputStream.writeObject(book1);
+            objectOutputStream.writeObject(book2);
+        }
+
+        ArrayList<Zh_Books> bookList = bookService.getBooks("testBooks.dat");
+        assertEquals(2, bookList.size());
+        assertEquals("Book 1", bookList.get(0).getTitle());
+        assertEquals("Author 2", bookList.get(1).getISBN());
+        assertEquals(2024, bookList.get(1).getQuanity());
+    }
 
 
-//    @Test
-//    public void testUpdateBook() {
-//        // Mocking BookFunctionalities
-//        BookService mockedFunctionalities = Mockito.mock(BookService.class);
-//        ArrayList<Zh_Books> mockedBooks = new ArrayList<>();
-//        Zh_Books bookToUpdate = new Zh_Books("123", "Updated Title", 2);
-//        mockedBooks.add(new Zh_Books("123", "Title", 1)); // Adding an initial book to the mocked list
-//
-//
-//        // Mock writeBooksInFile method
-//        doNothing().when(mockedFunctionalities).writeBooksInFile(any());
-//
-//        // Using the mocked BookFunctionalities in BookService
-//        BookFunctionalities bookService = new BookFunctionalities(mockedFunctionalities);
-//        ArrayList<Zh_Books> result = bookService.UpdateBook(bookToUpdate);
-//
-//        // Assertions
-//        assertEquals("Updated Title", result.get(0).getTitle()); // Check if the book title is updated
-//
-//        // Verify that getBooks method is called once
-//        Mockito.verify(mockedFunctionalities, Mockito.times(1)).getBooks(Mockito.any());
-//
-//        // Verify that writeBooksInFile method is called once
-//        Mockito.verify(mockedFunctionalities, Mockito.times(1)).writeBooksInFile(any());
-//    }
+    @Test
+    public void testUpdateBook() {
+        BooksSreviceMock booksSreviceMock = new BooksSreviceMock();
+        BookFunctionalities bookFunctionalities = new BookFunctionalities(booksSreviceMock);
+        Zh_Books books = new Zh_Books();
+        books.setISBN("11");
+        books.setQuanity(12);
+        ArrayList<Zh_Books> updatedBooks =   bookFunctionalities.UpdateBook(books);
+        assertEquals(updatedBooks.get(0).getQuanity(),books.getQuanity() );
+
+
+    }
 
 
 

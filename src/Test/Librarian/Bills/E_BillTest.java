ebf75d9456a6b77a2336f1b21437e0a9e103a3f7
EraMulla <emulla21@epoka.edu.al>
2023-12-19 20:55:30 +0100

    prova

--- src/Test/Librarian/Bills/E_BillTest.java
+++ src/Test/Librarian/Bills/E_BillTest.java
@@ -2,16 +2,17 @@ package Test.Librarian.Bills;
 
 
 import Librarian.Bill;
+import Librarian.Zh_MyDate;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class E_BillTest {
 
-    @Test
-    void setBook_name() {
 
-    }
 
     @Test
     void getLibrarian_Name() {
@@ -22,9 +23,7 @@ class E_BillTest {
 
     }
 
-    @Test
-    void setLibrarian_Name() {
-    }
+
 
     @Test
     void getBookquantity() {
@@ -41,34 +40,53 @@ class E_BillTest {
 
     @Test
     void getPrice() {
+        Bill bill2 = new Bill(20.0,new Zh_MyDate(2,3,2000));
+        Bill bill = new Bill();
+        bill.setPrice(25.9);
+        double actual = bill.getPrice();
+        assertEquals(25.9,actual);
     }
 
-    @Test
-    void setPrice() {
-    }
 
     @Test
     void getBook_name() {
+        Bill bill = new Bill();
+        ArrayList<String> bookNames = new ArrayList<>();
+        bookNames.add("Era");
+        bookNames.add("Greisi");
+        bill.setBook_name(bookNames);
+        ArrayList<String> actual = bill.getBook_name();
+        assertEquals(bookNames,actual);
+
     }
 
     @Test
     void getDate() {
+        Zh_MyDate date = new Zh_MyDate();
+        date.setDay(9);
+        date.setMonth(1);
+        date.setYear(2000);
+        Bill bill = new Bill();
+        bill.setDate(date);
+        Zh_MyDate actual = bill.getDate();
+        assertEquals(date,actual);
     }
 
-    @Test
-    void setDate() {
-    }
 
     @Test
     void getName() {
-    }
-
-    @Test
-    void setName() {
+        Bill bill = new Bill();
+        bill.setName("Era");
+        String actual = bill.getName();
+        assertEquals("Era",actual);
     }
 
     @Test
     void getSurname() {
+        Bill bill = new Bill();
+        bill.setSurname("Mulla");
+        String actual = bill.getSurname();
+        assertEquals("Mulla",actual);
     }
 
     @Test
